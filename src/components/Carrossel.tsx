3243b885f92dd236e7b246e6847efbd18ea764c0
WisleyASousa <wisleya.sousa@outlook.com.br>
2022-11-28 18:42:39 -0300

    Finalizando header, main, footer, iniciando parte visual

--- src/components/Carrossel.tsx
+++ src/components/Carrossel.tsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Carrossel() {
     return (
-        <div>
+        <div className="pt-4">
             <div id="carouselExampleDark" className="carousel pt-5 carousel-dark slide" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -39,41 +39,3 @@ export default function Carrossel() {
         </div>
     )
 }
-
-
-
-{/* <div id="carouselExampleCaptions" className="carousel slide" data-interval="3000" data-ride="carousel">
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active" data-bs-interval={10000}>
-                            <img  
-                                src='../../img/carrossel-img1.jpg'
-                                className={`d-block w-100 ${style.imagemCarrossel}`}alt="..." 
-                            />
-                                <div className="carousel-caption d-none d-md-block h-50">
-                                    <h1 className={`${style.fonteTitulo} display-4`} >Reaproveite melhor os alimentos!</h1>
-                                </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img 
-                                src='../../img/carrossel-img2.jpg' 
-                                className={`d-block w-100 ${style.imagemCarrossel}`} 
-                                alt="..." 
-                            />
-                                <div className="carousel-caption d-none d-md-block h-50">
-                                    <h1 className={`${style.fonteTitulo} display-4`}>Economize e ganhe em saúde!</h1>
-                                </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div> */}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,31 @@ export default function Header() {
   return (
       <div>
           <header>
-              <nav className={`navbar navbar-expand-lg navbar-dark bg-dark px-4 navbar-light fixed-top bg-light`}>
-                  <a className={`navbar-brand bold ${style.fonteTitulo} ${style.corEspecial}`} href="#">Learn Plus</a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                      <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                      <ul className="navbar-nav">
-                          <li className="nav-item active">
-                              <a className="nav-link" href="#">Início<span className="sr-only"></span></a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#">Cursos</a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#">Quem somos</a>
-                          </li>
-                          <li className="nav-item">
-                              <a className={`nav-link bold ${style.cor}`} data-toggle="modal" data-target="#modal-contato" href="#">Assine</a>
-                          </li>
-                      </ul>
+              <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark bg-light">
+                  <div className="container-fluid ">
+                      <a className={`navbar-brand  bold ${style.fonteTitulo} fs-2 ${style.corEspecial}`} href="#">Learn Plus</a>
+                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                          <span className="navbar-toggler-icon"></span>
+                      </button>
+                      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                          <ul className="navbar-nav">
+                              <li className="nav-item">
+                                <a className="nav-link active" href="#">Início<span className="sr-only"></span></a>
+                              </li>
+                              <li className="nav-item">
+                                <a className="nav-link" href="#">Cursos</a>
+                              </li>
+                              <li className="nav-item">
+                                <a className="nav-link" href="#">Quem somos</a>
+                              </li>
+                              <li className="nav-item">
+                                <a className={`nav-link bold ${style.cor}`}  href="#">Assine</a>
+                              </li>
+                              <li className="nav-item">
+                                <a className="nav-link" href="#">Login</a>
+                              </li>
+                          </ul>
+                      </div>
                   </div>
               </nav>
           </header>
